e09af0f3829e373c64fbea7e435be338ccb51413
AlexisEV <101740078+AlexisEV@users.noreply.github.com>
2022-10-30 22:34:17 -0500

    cambio alexis 2

--- CPartida.h
+++ CPartida.h
@@ -1,18 +1,21 @@
-#ifndef  CPARTIDA_H
-#define  CPARTIDA_H
+#ifndef CPARTIDA_H
+#define CPARTIDA_H
+
 #include "Header.h"
 #include "CTabla.h"
 
-class CPartida
-{
-  CTabla tablero;
-  int opcion;
-  string posOrigen;
-  string posDestino;
-  bool turno; //true: si la pieza es blanca 
-              //false : si la pieza es negra
-public : 
-CPartida;
-~CPartida() : default;
+class CPartida {
+    CTabla tablero;
+    int opcion;
+    string posOrigen;
+    string posDestino;
+    bool turno;  // true: Piezas blancas, false: Piezas negras
+public:
+    CPartida();
+    ~CPartida() = default;
+    void anunciarGanador() const;
+    void ejecutarMovimiento();
+    void ejecutarPartida();
+};
 
-}
+#endif
